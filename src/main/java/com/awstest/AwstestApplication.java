23cdd18df81a687766d126ffea187b5d33bd9c09
seunghwanLee <dkqlch@naver.com>
2023-02-28 19:34:36 +0900

    Boot 프로젝트 Docker Image를 이용한 AWS EC2 배포준비

--- src/main/java/com/awstest/AwstestApplication.java
+++ src/main/java/com/awstest/AwstestApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@PropertySource("classpath:/aws.properties")
 public class AwstestApplication {
 
 	public static void main(String[] args) {
